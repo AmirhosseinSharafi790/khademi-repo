c034c474ad741ad16b77e926894da72c93f9b9ff
amirKarami <amirrezakarami008@gmail.com>
2024-05-29 17:26:08 +0330

    swiper

--- src/pages/Album/Swipers.js
+++ src/pages/Album/Swipers.js
@@ -6,36 +6,32 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 function Swipers() {
-    let Items = [
-        {
-            SwiperNumber = {[
-                {
-                    id : 1,
-                    image : 'https://swiperjs.com/demos/images/nature-1.jpg'
-                },
-                {
-                    id : 1,
-                    image : 'https://swiperjs.com/demos/images/nature-1.jpg'
-                },
-                {
-                    id : 1,
-                    image : 'https://swiperjs.com/demos/images/nature-1.jpg'
-                },
-                {
-                    id : 1,
-                    image : 'https://swiperjs.com/demos/images/nature-1.jpg'
-                },
-                {
-                    id : 1,
-                    image : 'https://swiperjs.com/demos/images/nature-1.jpg'
-                },
-                {
-                    id : 1,
-                    image : 'https://swiperjs.com/demos/images/nature-1.jpg'
-                },
-            ]},
-        }
-      ];
+  let Items = [
+    {
+      id: 1,
+      image: "https://swiperjs.com/demos/images/nature-1.jpg",
+    },
+    {
+      id: 2,
+      image: "https://swiperjs.com/demos/images/nature-2.jpg",
+    },
+    {
+      id: 3,
+      image: "https://swiperjs.com/demos/images/nature-3.jpg",
+    },
+    {
+      id: 4,
+      image: "https://swiperjs.com/demos/images/nature-4.jpg",
+    },
+    {
+      id: 5,
+      image: "https://swiperjs.com/demos/images/nature-5.jpg",
+    },
+    {
+      id: 6,
+      image: "https://swiperjs.com/demos/images/nature-6.jpg",
+    },
+  ];
   return (
     <>
       <h1 className="text-center py-3">سفر جذاب مهریز مقدس</h1>
